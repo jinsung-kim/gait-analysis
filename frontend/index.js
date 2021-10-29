8e593443d170004dc358d50d5959e1bb1e3f809f
Jin Kim <jinsungkim1210@gmail.com>
2021-10-28 20:10:25 -0400

    refactor: changing to express/node html stack

---
+++ frontend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html'); // Used to load the site
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
