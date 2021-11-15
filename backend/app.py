35de7161ac35f1c5ad34784653a9c7f48eefa449
Jin Kim <jinsungkim1210@gmail.com>
2021-11-15 15:51:35 -0500

    fixing heat map

--- backend/app.py
+++ backend/app.py
@@ -62,8 +62,7 @@ def getBoxPlotData():
     right = {}
 
     if data:
-        # target_id = int(data["id"])
-        target_id = 380554
+        target_id = int(data["id"])
 
         user_sessions = {}
         res = {}
@@ -106,11 +105,17 @@ def getHeatMapData():
     re = []
 
     for session in SESSIONS.find():
-        if (session["user"] == 1): # Should be all sessions for now
-            d = datetime.strptime(session["date"], "%m/%d/%Y")
+        if (str(session["user"]) == "618877e7ff9832fe99b10cd0"): # Should be all sessions for now
+            d = datetime.strptime(session["date"], "%m-%d-%Y")
             c = days_between(today, d)
-            by_date[c] = { "left": session["left"], "right": session["right"] }
-            m = max(m, c)
+
+            # To address multiple sessions on the same day
+            if c not in by_date:
+                by_date[c] = [{ "left": session["left"], "right": session["right"] }]
+            else:
+                by_date[c].append({ "left": session["left"], "right": session["right"] })
+            
+            m = max(m, c) # Furthest back
             re.append({ "date": session["date"], "left": session["left"], "right": session["right"] })
 
     by_week = {} # Where all of the sessions are stored by week (used to average)
@@ -120,55 +125,58 @@ def getHeatMapData():
     for i in range(m):
         if i in by_date:
             week = math.floor(i / 7) # To associate the date with a week
-            if week in by_week:
-                by_week[week].append(by_date[i])
-            else:
-                by_week[week] = [by_date[i]]
-                last_week = max(last_week, week)
+            for j in range(len(by_date[i])):
+                if week in by_week:
+                    by_week[week].append(by_date[i][j])
+                else:
+                    by_week[week] = [by_date[i][j]]
+                    last_week = max(last_week, week) + 1
 
     avgs = {}
 
     for week in range(last_week):
+
         if week not in by_week:
             continue
+
         ses_c = len(by_week[week]) # Number of sessions in this week
 
         gait_l = 0
-        cad_l = 0
-        stride_l = 0
+        str_l = 0
+        step_l = 0
 
         gait_r = 0
-        cad_r = 0
-        stride_r = 0
+        str_r = 0
+        step_r = 0
 
         for i in range(ses_c): # Each session of the week
-            gait_l += by_week[week][i]["left"]["gait_vel"]["med"]
-            cad_l += by_week[week][i]["left"]["cadence"]["med"]
-            stride_l += by_week[week][i]["left"]["stride_pace"]["med"]
+            gait_l += by_week[week][i]["left"]["stride_pace"]["median"]
+            str_l += by_week[week][i]["left"]["stride_length"]["median"]
+            step_l += by_week[week][i]["left"]["step_rate"]["median"]
 
-            gait_r += by_week[week][i]["right"]["gait_vel"]["med"]
-            cad_r += by_week[week][i]["right"]["cadence"]["med"]
-            stride_r += by_week[week][i]["right"]["stride_pace"]["med"]
+            gait_r += by_week[week][i]["right"]["stride_pace"]["median"]
+            str_r += by_week[week][i]["right"]["stride_length"]["median"]
+            step_r += by_week[week][i]["right"]["step_rate"]["median"]
 
         # Gather weekly averages
         gait_l /= ses_c
-        cad_l /= ses_c
-        stride_l /= ses_c
+        str_l /= ses_c
+        step_l /= ses_c
 
         gait_r /= ses_c
-        cad_r /= ses_c
-        stride_r /= ses_c
+        str_r /= ses_c
+        step_r /= ses_c
 
         left = {
             "gait_velocity": gait_l,
-            "stride_length": stride_l,
-            "step_rate": cad_l,
+            "stride_length": str_l,
+            "step_rate": step_l,
         }
 
         right = {
             "gait_velocity": gait_r,
-            "stride_length": stride_r,
-            "step_rate": cad_r,
+            "stride_length": str_r,
+            "step_rate": step_r,
         }
 
         avgs[week] = { "left": left, "right": right }
@@ -176,7 +184,7 @@ def getHeatMapData():
     res = []
 
     for session in re:
-        d = datetime.strptime(session["date"], "%m/%d/%Y")
+        d = datetime.strptime(session["date"], "%m-%d-%Y")
         c = days_between(today, d)
         week = math.floor(c / 7)
 
@@ -193,12 +201,13 @@ def getHeatMapData():
         # s_r = avgs[week]["right"]["stride_length"]
 
         # Comparing to the current session's data
-        g_diff = abs(g_l - session["left"]["gait_vel"]["med"]) + abs(g_r - session["right"]["gait_vel"]["med"])
-        # c_diff = ()
-        # s_diff = ()
+        g_diff = abs(g_l - session["left"]["stride_pace"]["median"]) + abs(g_r - session["right"]["stride_pace"]["median"])
+        # c_diff = abs(c_l - session["left"]["step_rate"]["median"]) + abs(c_r - session["right"]["step_rate"]["median"])
+        # s_diff = abs(s_l - session["left"]["stride_length"]["median"]) + abs(s_r - session["right"]["stride_length"]["median"])
 
-        res.append({ "date": session["date"], "val": g_diff })
+        res.append({ "date": session["date"].replace('-', '/'), "val": g_diff })
 
+    print(res)
 
     return jsonify(res)
 
